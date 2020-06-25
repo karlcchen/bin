0796df948490f5f85cce68bebf5635f86d093590
Karl Chen <kchen@sonicwall.com>
2020-06-25 13:21:35 -0700

    update asc ascii-esc.sh

--- ascii-esc.sh
+++ ascii-esc.sh
@@ -9,20 +9,22 @@
 AESC_RST_ALL="\e[0m"
 
 # not working, always became underlined
-#AESC_SET_BOLD="\e[1m"
-#AESC_RST_BOLD="\e[21m"
+AESC_SET_BOLD="\e[1m"
+# unbold not always supoorted, became double underlined in other caseasc 
+#AESC_RST_BOLD="\e[21m"  
+AESC_SET_D_UNDERLINED="\e[21m"  
 
 AESC_SET_DIM="\e[2m"
-AESC_RST_DIM="\e[22m"
+AESC_SET_NORM="\e[22m"
 
 AESC_SET_UNDERLINED="\e[4m"
 AESC_RST_UNDERLINED="\e[24m"
 
-AESC_SET_REVERSE="\e[7m"
-AESC_RST_REVERSE="\e[27m"
-
 AESC_SET_BLINK="\e[5m"
 AESC_RST_BLINK="\e[25m"
+
+AESC_SET_REVERSE="\e[7m"
+AESC_RST_REVERSE="\e[27m"
 
 # default foreground color
 AESC_COLOR_RST_FG="\e[39m"
