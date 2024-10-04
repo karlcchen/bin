0052c9a438258e55c5d8fd68174bb7c05750925d
Karl Chen <karlcchen@hotmail.com>
2024-10-03 20:13:58 -0700

    change default to using cmake/make

--- west.sh
+++ west.sh
@@ -1,27 +1,60 @@
 #!/bin/bash
 #
 
-VERSION="1.5.0"
+VERSION="1.7.0"
 SAVED_WEST_PROJECT_FILE="saved_west_projects.txt"
 BOARD_DIR="./boards"
 PWD=`pwd`
-BIN_DEST_DIR="./BIN-Firmware"
-BUILD_DIR="./build"
-BIN_SPEC="bin"
+BIN_FIRST_NAME="zephyr"
+BIN_DEST_DIR="BIN-Firmware"
+BUILD_DIR="build"
+
+#
+# hex and elf file is much smaller if address is not contigeous
+#BIN_SPEC="bin"
+BIN_SPEC="elf"
 HEX_SPEC="hex"
 EXE_SPEC="exe"
-ELF_SPEC="elf"
+
+#
+#BUILD_MAKER="west"
+BUILD_MAKER="cmake"
+b_DRY_RUN=0
+b_NEW_BUILD=0
 
 if [[ -z "$1" ]] ; then 
-    printf '\nUsage: %s [BOARD_Name] PRJ_PATH\n' "$0" 
-    printf 'BOARD examples: stm32h7s78_dk black_f407ve rpi_4b rpi_5\n'
-    printf 'PRJ_PATH examples: ./sample/basic/blinky\n\n'
-    prinrf 'use \"-b\" as Board Name and \"-p\" as Project Path from (saved) file: \"%s\"\n' "${SAVED_WEST_PROJECT_FILE}"
+    printf '\nUsage: %s [BOARD_Name] PRJ_PATH  [--new]\n\n' "$0" 
+    printf 'Example of BOARD: stm32h7s78_dk black_f407ve rpi_4b rpi_5\n'
+    printf 'Note: use \"-b\" or \"-l\" as Board Name and \"-p\" or \"-l\" as Project Path from (saved) file: \"%s\"\n\n' "${SAVED_WEST_PROJECT_FILE}"
+    printf 'Examples:\n'
+    printf '\t west.sh f407ve sample/basic/blinky --new\n'
+    printf '\t west.sh stm32h7s78_dk samples/application_development/code_relocation_nocopy/\n'
+    printf '\t west.sh s78 -l\n'
+    printf '\t west.sh --west -l samples/subsys/portability/cmsis_rtos_v2/philosophers\n'
+    printf '\t west.sh --dry s78 samples/subsys/fs/fs_sample %s\n' "-DCONF_FILE=prj_ext.conf"
+    printf '\n%s Version: %s\n' "$0" "${VERSION}"  
     exit 1
 fi 
 if [[ "$1" = "--version" ]] ; then 
     printf '%s Version: %s\n' "$0" "${VERSION}"  
     exit 0 
+fi
+
+if [[ "$1" = "--dry" ]] ; then 
+    b_DRY_RUN=1
+    shift 1
+fi
+if [[ "$1" = "--new" ]] ; then 
+    b_NEW_BUILD=1
+    shift 1
+fi
+if [[ "$1" = "--west" || "$1" = "--ninja" ]] ; then 
+    BUILD_MAKER="west"
+    shift 1
+fi
+if [[ "$1" = "--cmake" ]] ; then 
+    BUILD_MAKER="cmake"
+    shift 1
 fi
 
 if [[ -f "${SAVED_WEST_PROJECT_FILE}" ]] ; then 
@@ -72,7 +105,6 @@ elif [[ "${BOARD}" == "-b" || "${BOARD}" == "-l" ]] ; then
         exit 9
     fi 
     BOARD="${LAST_BOARD}"
-    shift 1   
 fi
 
 # check board is valid using "west boards"
@@ -114,7 +146,6 @@ elif [[ "${PRJ_PATH}" == "-p" || "${PRJ_PATH}" == "-l" ]] ; then
         exit 9
     fi 
     PRJ_PATH="${LAST_PRJ_PATH}"
-    shift 1
 fi
 
 if [[ ! -d "${BOARD_DIR}" ]] ; then
@@ -144,6 +175,44 @@ else
     popd >> /dev/null
 fi 
 
+TMP_LOG=".tmp-${PRJ_PREV_DIR}-${PRJ_PATH_BASENAME}.log"
+if [ -e "${TMP_LOG}" ] ; then 
+    printf '### INFO: Removing Project Build Log File: %s\n' "${TMP_LOG}"
+    rm ${TMP_LOG}  
+fi
+DATE_STR=`date`
+printf '\nDATE: %s\n\n' "${DATE_STR}" >> ${TMP_LOG}
+
+if [[ -n "$1" && "$1" == "--new" ]] ; then 
+    b_NEW_BUILD=1
+    shift 1
+fi
+if [[ b_NEW_BUILD -eq 1 ]] ; then
+    printf '\n### INFO: REMOVE BUILD DIR: %s\n' "./${BUILD_DIR}/"
+    rm -rf ./${BUILD_DIR}/
+    if [[ $? -ne 0 ]] ; then 
+        printf '\nERROR: rm -rf %s failed!\n' "./${BUILD_DIR}/"
+        exit 9
+    fi 
+fi
+
+if [[ ! -z "$1" ]] ; then 
+    printf '### INFO: west extra argument:' 
+    echo $@
+fi
+
+if [[ -n "$1" && "$1" == "--dry" ]] ; then 
+    b_DRY_RUN=1
+    shift 1
+fi
+if [[ b_DRY_RUN -eq 1 ]] ; then
+    echo 
+    echo "--dry run command:"
+    echo " west build -p always -b ${BOARD} ${PRJ_PATH} $@ | tee ${TMP_LOG}"
+    echo
+    exit 0 
+fi
+
 # save board name and project path to file
 if [[ "${LAST_BOARD}" != "${BOARD}" || "${LAST_PRJ_PATH}" != "${PRJ_PATH}" ]] ; then
     printf '%s\t%s\n' "${BOARD}" "${PRJ_PATH}" >> ${SAVED_WEST_PROJECT_FILE}
@@ -154,50 +223,45 @@ if [[ "${LAST_BOARD}" != "${BOARD}" || "${LAST_PRJ_PATH}" != "${PRJ_PATH}" ]] ; 
     printf '### INFO: "%s\t%s" saved to file: %s\n' "${BOARD}" "${PRJ_PATH}" "${SAVED_WEST_PROJECT_FILE}"
 fi
 
-TMP_LOG=".tmp-${PRJ_PREV_DIR}-${PRJ_PATH_BASENAME}.log"
-if [ -e "${TMP_LOG}" ] ; then 
-    printf '### INFO: Removing Project Build Log File: %s\n' "${TMP_LOG}"
-    rm ${TMP_LOG}  
-fi
-DATE_STR=`date`
-printf '\nDATE: %s\n\n' "${DATE_STR}" >> ${TMP_LOG}
-
-if [[ -n "$1" && "$1" == "--new" ]] ; then 
-    printf '\n### INFO: REMOVE BUILD DIR: %s\n' "${BUILD_DIR}"
-    rm -rf ${BUILD_DIR}
-    if [[ $? -ne 0 ]] ; then 
-        printf '\nERROR: rm -rf %s failed!\n' "${BUILD_DIR}"
-        exit 9
+if [[ "${BUILD_MAKER}" == "west" ]] ; then 
+    ${BUILD_MAKER} build -p always -b ${BOARD} ${PRJ_PATH} $@ | tee ${TMP_LOG}
+    PIPE_STA="`echo "${PIPESTATUS[@]}"`"
+elif [[ "${BUILD_MAKER}" == "cmake" ]] ; then 
+    ${BUILD_MAKER} -B${BUILD_DIR} -DBOARD=${BOARD} ${PRJ_PATH} $@ | tee ${TMP_LOG}
+    PIPE_STA="`echo "${PIPESTATUS[@]}"`"
+    if [[ "${PIPE_STA}" != "0 0" ]] ; then
+        printf '\nERROR: %s failed PIPED Status: %s\n' "${BUILD_MAKER}" "${PIPE_STA}"
+        mv ${TMP_LOG} ${TMP_LOG}.ERR
+        exit 6 
     fi 
-    shift 1
+    make -C ${BUILD_DIR} -j | tee -a ${TMP_LOG}
+    PIPE_STA="`echo "${PIPESTATUS[@]}"`"
 fi
-
-if [[ ! -z "$1" ]] ; then 
-    printf '\n### INFO: west extra argument 1=%s, 2=%s, 3=%s\n' "$1" "$2" "$3"
-fi
-west build -p always -b ${BOARD} ${PRJ_PATH} $1 $2 $3 | tee ${TMP_LOG}
-PIPE_STA="`echo "${PIPESTATUS[@]}"`"
 if [[ "${PIPE_STA}" != "0 0" ]] ; then
-    printf '\nERROR: west failed status: %s\n' "${PIPE_STA}"
+    printf '\nERROR: %s failed, Piped Status: %s\n' "${BUILD_MAKER}" "${PIPE_STA}"
     mv ${TMP_LOG} ${TMP_LOG}.ERR
     exit 6 
 fi 
 
-printf '\nINFO: build completed, find Binary Output %s under PATH %s\n' "${BUILD_DIR}" "*.${BIN_SPEC}"
-BIN_FILE=`find ${BUILD_DIR} -type f -name "*.${BIN_SPEC}"`
-HEX_FILE=`find ${BUILD_DIR} -type f -name "*.${HEX_SPEC}"`
+printf '\n### INFO: Build completed, find Binary File under Dir: \"%s\", Name: \"%s\"\n' "./${BUILD_DIR}" "${BIN_FIRST_NAME}.${BIN_SPEC}"
+HEX_FILE=`find ./${BUILD_DIR} -type f -name "${BIN_FIRST_NAME}.${HEX_SPEC}"`
 if [[ $? -ne 0 ]] ; then
-    printf '\nERROR: find Binary File (%s) failed!\n' "*.${BIN_SPEC}"
+    printf '\nERROR: No Binary File (%s) generated!\n' "${BIN_FIRST_NAME}.${HEX_SPEC}"
+    exit 7 
+fi 
+BIN_FILE=`find ./${BUILD_DIR} -type f -name "${BIN_FIRST_NAME}.${BIN_SPEC}"`
+if [[ $? -ne 0 ]] ; then
+    printf '\nERROR: No Binary File \"%s\" generated!\n' "${BIN_FIRST_NAME}.${BIN_SPEC}"
     exit 7 
 fi 
 if [[ ! -z "${BIN_FILE}" ]] ; then
 	printf '\n#####  build complete, %s file found: %s\n' "*.${BIN_SPEC}" "${BIN_FILE}"
-        BIN_FILE=`realpath ${BIN_FILE}`
+        BIN_FILE="`realpath ${BIN_FILE}`"
         SAVED_BIN_FILE="zephyr-${BOARD}-${PRJ_PREV_DIR}_${PRJ_PATH_BASENAME}.${BIN_SPEC}"
         SAVED_HEX_FILE="zephyr-${BOARD}-${PRJ_PREV_DIR}_${PRJ_PATH_BASENAME}.${HEX_SPEC}"
-        cp ${BIN_FILE} ${BIN_DEST_DIR}/${SAVED_BIN_FILE}
+        cp ${BIN_FILE} ./${BIN_DEST_DIR}/${SAVED_BIN_FILE}
         if [[ $? -ne 0 ]] ; then
-            printf '\nERROR: cp binaey file %s to Dir %s failed!\n' "${SAVED_BIN_FILE}" "${BIN_DEST_DIR}}"
+            printf '\nERROR: cp binaey file %s to Dir %s failed!\n' "${SAVED_BIN_FILE}" "./${BIN_DEST_DIR}"
             exit 8 
         fi 
         cp ${HEX_FILE} ${BIN_DEST_DIR}/${SAVED_HEX_FILE}
@@ -212,7 +276,7 @@ if [[ ! -z "${BIN_FILE}" ]] ; then
         printf '\n### Hex Saved: %s\n' "${SAVED_HEX_FILE_FULLPATH}"
 else
 	printf '\n =====   WARNING: No .bin file found!   =====\n'
-        EXE_FILE=`find ${BUILD_DIR} -type f -name "*.${EXE_SPEC}"`
+        EXE_FILE=`find ./${BUILD_DIR} -type f -name "*.${EXE_SPEC}"`
         if [[ $? -ne 0 ]] ; then
             printf '\nERROR: find Executable file %s failed!\n' "*.${EXE_SPEC}"
             exit 9 
